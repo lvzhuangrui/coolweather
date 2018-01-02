cbf689d0d4ac1187ae6500d151de15c2feebc8da
LBX <649699870@qq.com>
2018-01-02 14:17:12 +0800

    加载第三方库和数据库表

---
+++ app/src/main/java/com/example/belief/coolweather/db/City.java
@@ -0,0 +1,46 @@
+package com.example.belief.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Belief on 2018/1/2.
+ */
+
+public class City extends DataSupport {
+    private int id;
+    private String cityName;
+    private String weatherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
